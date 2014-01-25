26343d66f751cd22b64d204d9f495a2c80fb5fcc
Max Prokopiev <me@maxprokopiev.com>
2014-01-25 16:52:12 +0400

    Remove :registerable. Fix devise layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  layout :layout_by_resource
+
+  private
+
+  def layout_by_resource
+    devise_controller? ? 'admin' : 'application'
+  end
 end
