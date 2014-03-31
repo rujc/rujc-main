bc88d50b050b157c5ff8076eaa6cde57b1964d3a
Vasily Polovnyov <vasily@polovnyov.ru>
2014-03-31 20:57:20 +0400

    sketch next event date jumper

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,8 @@
 class Event < ActiveRecord::Base
+  scope :ordered, -> { order('events.starts_at asc') }
+  scope :since, ->(date) { ordered.where('events.starts_at > ?', date) }
+
+  def next_in_schedule
+    self.class.since(starts_at).first
+  end
 end
