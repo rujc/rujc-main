4db89c63c7861ec154693c5d6de957a65d6ad9d8
Vasily Polovnyov <vasily@polovnyov.ru>
2014-03-31 21:09:20 +0400

    add nice bg to next event link

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -5,7 +5,7 @@ module EventsHelper
 
   def event_next_in_schedule(event)
     if next_event = event.next_in_schedule
-      render 'events/next_event', event: event
+      render 'events/next_event', event: next_event
     end
   end
 end
