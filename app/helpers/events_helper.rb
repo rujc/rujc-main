cce7aadfe95b6e1daa7f7e226a1188cb6b531e9a
Vasily Polovnyov <vasily@polovnyov.ru>
2014-03-31 21:26:28 +0400

    display next day event in schedule

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -4,7 +4,7 @@ module EventsHelper
   end
 
   def event_next_in_schedule(event)
-    if next_event = event.next_in_schedule
+    if next_event = event.next_day_in_schedule
       render 'events/next_event', event: next_event
     end
   end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
   scope :ordered, -> { order('events.starts_at asc') }
   scope :since, ->(date) { ordered.where('events.starts_at > ?', date) }
 
-  def next_in_schedule
-    self.class.since(starts_at).first
+  def next_day_in_schedule
+    self.class.since(starts_at.end_of_day).first
   end
 end
