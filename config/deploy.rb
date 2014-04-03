a127a1b30fb3bdda7ae2408e825da86d61162952
Max Prokopiev <me@maxprokopiev.com>
2014-04-03 13:36:49 +0400

    Link config/unicorn.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :pty, true
 set :rbenv_ruby, '2.0.0-p451'
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/unicorn.rb}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
