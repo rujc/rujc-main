f32d523239abe086038f9d85ccdc3ee3d60758dd
Max Prokopiev <me@maxprokopiev.com>
2014-04-04 11:02:28 +0400

    Restart unicorn on deploy:restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,9 +7,6 @@ set :repo_url, 'git@github.com:juggler/rjc.git'
 
 set :branch, 'deploy'
 
-# Default value for :pty is false
-set :pty, true
-
 set :rbenv_custom_path, '/opt/rbenv'
 set :rbenv_ruby, '2.0.0-p451'
 
@@ -19,15 +16,14 @@ set :linked_files, %w{config/database.yml config/unicorn.rb}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :default_env, { path: "/opt/rbenv/bin:/opt/rbenv/shims:$PATH" }
 
 namespace :deploy do
 
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :service, 'unicorn_rjc restart'
     end
   end
 
