8d79c8a11e5ef2a9a9b20a8b395d1086bf499938
Max Prokopiev <me@maxprokopiev.com>
2014-04-06 22:25:54 +0400

    Apply review comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rjc::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   root to: 'static#main'
 
   resources :events, only: [:index, :show]
