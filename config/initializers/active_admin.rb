eaab18a22bd111fe9e2d4bafdf6653cacc50db7f
Max Prokopiev <me@maxprokopiev.com>
2014-04-06 22:46:55 +0400

    Fix typos and udpate schema.rb

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -203,26 +203,10 @@ ActiveAdmin.setup do |config|
   #   end
 
 
-  # == Download Links
-  #
-  # You can disable download links on resource listing pages,
-  # or customize the formats shown per namespace/globally
-  #
-  # To disable/customize for the :admin namespace:
-  #
-  #   config.namespace :admin do |admin|
-  #
-  #     # Disable the links entirely
-  #     admin.download_links = false
-  #
-  #     # Only show XML & PDF options
-  #     admin.download_links = [:xml, :pdf]
-  #
-  #     # Enable/disable the links based on block
-  #     #   (for example, with cancan)
-  #     admin.download_links = proc { can?(:view_download_links) }
-  #
-  #   end
+  # Disable download links on resource listing pages,
+  config.namespace :admin do |admin|
+    admin.download_links = false
+  end
 
 
   # == Pagination
