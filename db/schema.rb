c0f2b1784feb84ff306abb7c50054de88f49cf85
Max Prokopiev <me@maxprokopiev.com>
2014-04-06 22:01:29 +0400

    Add places to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140406175243) do
+ActiveRecord::Schema.define(version: 20140406175759) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20140406175243) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "conditions",  default: "free", null: false
+    t.integer  "place_id"
   end
 
   create_table "places", force: true do |t|
