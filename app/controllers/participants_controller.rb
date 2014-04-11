e1d364057e741a7102820cb6a19452cbfd9ed984
Max Prokopiev <me@maxprokopiev.com>
2014-04-11 18:13:38 +0400

    I18n for signup

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -2,10 +2,10 @@ class ParticipantsController < ApplicationController
   def create
     participant = Participant.new(participant_params)
     if participant.save
-      redirect_to :back, notice: 'Регистрация прошла успешно, на ваш e-mail было выслано письмо с дальнейшими инструкциями.'
+      redirect_to :back, notice: I18n.t('app.signup.success')
     else
       first_error = participant.errors.first.last
-      redirect_to :back, alert: "При регистрации произошла ошибка — #{first_error}, пожалуйста, повторите попытку."
+      redirect_to :back, alert: I18n.t('app.signup.failure', error: first_error)
     end
   end
 
