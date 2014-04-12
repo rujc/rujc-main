45e0af3eeb1ca94e152163fe4fe8ba583d1b2bc8
Max Prokopiev <me@maxprokopiev.com>
2014-04-12 07:58:19 +0400

    Link uploads on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :rbenv_ruby, '2.1.1'
 set :linked_files, %w{config/database.yml config/unicorn.rb}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{public/uploads}
 
 set :default_env, { path: "/opt/rbenv/bin:/opt/rbenv/shims:$PATH" }
 
