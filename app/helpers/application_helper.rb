1a40f0328f8c2c7230d000a5b4a80fee014620ec
Max Prokopiev <me@maxprokopiev.com>
2014-05-11 15:52:20 +0400

    Add event participation links via twitter web intents

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,22 @@ module ApplicationHelper
       id: 'flash_messages',
       class: messages.blank? ? 'hidden' : ''
   end
+
+  def twitter_web_intent_url(text)
+    [
+      'https://twitter.com/intent/tweet?hashtags=rjc2014&via=rjc_2014&text=Я пойду на ',
+      URI.escape(text)
+    ].join
+  end
+
+  def twitter_message(event)
+    [event.title, '|', event_url(event)].join(' ')
+  end
+
+   def event_participate_link(event)
+    message = twitter_message(event)
+    url = twitter_web_intent_url(message)
+
+    link_to 'Я пойду!', url, class: 'btn event-participate_link', target: '_blank'
+  end
 end
