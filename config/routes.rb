7495d0a455894be2adf35a8144f6a47688974b3c
Max Prokopiev <me@maxprokopiev.com>
2014-06-06 22:18:14 +0400

    Remove unused routes for now

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,7 @@ Rjc::Application.routes.draw do
 
   root to: 'static#main'
 
-  resources :events, only: [:index, :show] do
-    post :participants, on: :member
-  end
   resources :participants, only: [:create]
 
-  get '/creators', to: 'static#creators'
-  get '/contacts', to: 'static#contacts'
   get '/team',     to: 'static#team'
 end
