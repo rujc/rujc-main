8b99fdd629821a7407d64928ec098717ed6c6cf1
Max Prokopiev <me@maxprokopiev.com>
2014-10-20 20:33:34 +0400

    Fix title and body methods

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,13 +2,18 @@ class Article < ActiveRecord::Base
   validates :title, :body, presence: true
 
   def title
-    binding.pry
-    locale = I18n.locale == :ru ? "" : "_en"
-    public_send("title#{I18n.locale}")
+    if I18n.locale == :ru
+      super
+    else
+     public_send("title_#{I18n.locale}")
+    end
   end
 
   def body
-    locale = I18n.locale == :ru ? "" : "_en"
-    public_send("body#{I18n.locale}")
+    if I18n.locale == :ru
+      super
+    else
+     public_send("body_#{I18n.locale}")
+    end
   end
 end
