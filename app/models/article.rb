522417d9eb06ba345d3af5a1211c6aa11d4adcc4
Max Prokopiev <me@maxprokopiev.com>
2014-10-20 20:31:47 +0400

    Remove _ if locale is russian

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,12 +2,13 @@ class Article < ActiveRecord::Base
   validates :title, :body, presence: true
 
   def title
-    locale = I18n.locale == :ru ? "" : "en"
-    public_send("title_#{I18n.locale}")
+    binding.pry
+    locale = I18n.locale == :ru ? "" : "_en"
+    public_send("title#{I18n.locale}")
   end
 
   def body
-    locale = I18n.locale == :ru ? "" : "en"
-    public_send("body_#{I18n.locale}")
+    locale = I18n.locale == :ru ? "" : "_en"
+    public_send("body#{I18n.locale}")
   end
 end
